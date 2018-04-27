5e8437c9629b2eedce1c6940265c4ea80ef3df1c
shiincs <saint0227@naver.com>
2018-04-27 17:39:24 +0900

    npm, gulp, babel, lint installed

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "off"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+	"no-console": [
+	    "off"
+	],
+	"linebreak-style": [
+	    "off"
+	]
+    }
+};
