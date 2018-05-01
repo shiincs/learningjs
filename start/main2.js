c19730926277f586041b4b9f2d160d7b6a52315a
shiincs <saint0227@naver.com>
2018-05-01 17:26:24 +0900

    added crown & anchor game code

---
+++ start/main2.js
@@ -0,0 +1,13 @@
+const s = "HELLO";
+s.rating = 3;
+console.log(s.rating);
+
+const date = new Date();
+const specificDate = new Date(2018, 3, 31, 15, 45, 10, 441);
+console.log(date);
+console.log(specificDate);
+
+const n = 0;
+const nn = Boolean(n);
+
+console.log(nn);
